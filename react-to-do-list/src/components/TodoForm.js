66c4a6ec86eeb084ce55eeb9af478be028e74109
Oscar Jimenez <ojimenezlmpc@gmail.com>
2021-06-07 14:41:42 -0700

    functionality all set. User can create a task edit and remove it. Must style it now.

--- react-to-do-list/src/components/TodoForm.js
+++ react-to-do-list/src/components/TodoForm.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function TodoForm(props) {
     const [input, setInput] = useState('');
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
     
     const handleChange = e => {
         setInput(e.target.value);
@@ -11,7 +16,7 @@ function TodoForm(props) {
         e.preventDefault();
 
         props.onSubmit({
-            id: Math.floor(Math.random() * 10000), // add random id to task, can be refactored to avoid duplicate ids
+            id: Math.floor(Math.random() * 10000), // adds random id to task, can be refactored to avoid duplicate ids
             text: input
         });
         setInput('');
@@ -19,7 +24,7 @@ function TodoForm(props) {
 
     return (
         <form className='todo-form' onSubmit={handleSubmit}>
-            <input type='text' placeholder='Add a task' value={input} name='text' className='todo-input' onChange={handleChange}/>
+            <input type='text' placeholder='Add a task' value={input} name='text' className='todo-input' onChange={handleChange} ref={inputRef} />
             <button className='todo-button'>Add task</button>
         </form>
     )
